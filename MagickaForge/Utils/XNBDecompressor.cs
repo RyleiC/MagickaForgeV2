35794e20d81787734b172f579954f557f05b5859
Ryleii_ <cobaltrylei@gmail.com>
2024-12-19 16:12:37 -0600

    Moved XNBDecompressor to Utils

---
+++ MagickaForge/Utils/XNBDecompressor.cs
@@ -0,0 +1,32 @@
+using XNBDecomp;
+
+namespace MagickaForge.Utils
+{
+    public static class XNBDecompressor
+    {
+        public static Stream DecompressXNB(string path)
+        {
+            var stream = new MemoryStream();
+            var contentReader = ContentReader.Create(path);
+
+            ContentWriter contentWriter = new ContentWriter(stream, false, contentReader.fileVersion, contentReader.graphicsProfile);
+            try
+            {
+                for (int i = 0; i < contentReader.fileSize; i++)
+                {
+                    contentWriter.Write(contentReader.ReadByte());
+                }
+            }
+            catch
+            {
+                throw new InvalidDataException("Decompression failed!");
+            }
+
+            contentReader.Close();
+            contentWriter.FlushOutput();
+
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
